21ae26d96a7185e8b024e1404f3d6c949bcbecde
Pavithran Pathmarajah <pavithranpath@gmail.com>
2017-01-09 22:59:49 -0500

    Updated to list all songs, 5 second delay for api limits

---
+++ AppleMusicAdd/AppleMusicAdd/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AppleMusicAdd
+//
+//  Created by Pavi on 2017-01-01.
+//  Copyright © 2017 Pavi_Student. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
